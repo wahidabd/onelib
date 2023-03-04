99f63fa74679cf0ba86640e2df20019a83b83fbe
Abd Wahid <wahed.blog99@gmail.com>
2023-03-04 21:38:19 +0700

    feat: base application

--- library/src/main/java/com/wahidabd/library/presentation/BaseApplication.kt
+++ library/src/main/java/com/wahidabd/library/presentation/BaseApplication.kt
@@ -1,14 +1,22 @@
 package com.wahidabd.library.presentation
 
 import android.app.Application
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 
 abstract class BaseApplication : Application() {
 
-    abstract fun getDefineModule()
+    abstract fun getDefineModule(): List<Module>
     abstract fun initApp()
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            getDefineModule()
+        }
+
+        initApp()
     }
 
 }
---
+++ library/src/main/java/com/wahidabd/library/utils/constant/OneConstant.kt
@@ -0,0 +1,10 @@
+package com.wahidabd.library.utils.constant
+
+object OneConstant {
+
+    // otp
+    const val OTP_VIEW_TYPE_NONE = 0
+    const val OTP_VIEW_TYPE_UNDERLINE = 1
+    const val OTP_VIEW_TYPE_BORDER = 2
+
+}
