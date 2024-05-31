b040ec3cc012fe3e17c9d743817b1d4bc595c0be
Abd Wahid <wahed.blog99@gmail.com>
2024-05-31 22:39:35 +0700

    remove unused dependencies

--- library/src/main/java/com/wahidabd/library/data/libs/ApiService.kt
+++ library/src/main/java/com/wahidabd/library/data/libs/ApiService.kt
@@ -4,7 +4,6 @@ import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Converter
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 /**
@@ -18,59 +17,6 @@ import retrofit2.converter.gson.GsonConverterFactory
  * - Create a base Retrofit instance with a default Gson converter.
  */
 object ApiService {
-
-    /**
-     * Creates a reactive Retrofit service with a default Gson converter.
-     *
-     * @param T The service interface type.
-     * @param serviceClass The class of the service interface.
-     * @param okHttpClient An OkHttpClient instance for making HTTP requests.
-     * @param baseUrl The base URL for the HTTP requests.
-     * @return An implementation of the service interface.
-     */
-    fun <T> createReactiveService(
-        serviceClass: Class<T>,
-        okHttpClient: OkHttpClient,
-        baseUrl: String
-    ): T {
-        val gson = GsonBuilder().create()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .client(okHttpClient)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-
-        return retrofit.create(serviceClass)
-    }
-
-    /**
-     * Creates a reactive Retrofit service with a custom converter factory.
-     *
-     * @param T The service interface type.
-     * @param serviceClass The class of the service interface.
-     * @param okHttpClient An OkHttpClient instance for making HTTP requests.
-     * @param baseUrl The base URL for the HTTP requests.
-     * @param factory A custom converter factory for serialization and deserialization.
-     * @return An implementation of the service interface.
-     */
-    fun <T> createReactiveService(
-        serviceClass: Class<T>,
-        okHttpClient: OkHttpClient,
-        baseUrl: String,
-        factory: Converter.Factory
-    ): T {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .client(okHttpClient)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(factory)
-            .build()
-
-        return retrofit.create(serviceClass)
-    }
-
 
     /**
      * Creates a non-reactive Retrofit service with a default Gson converter.
